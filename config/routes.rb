ca95ef40436d42007cb439f1fab7a27453f48bb5
Matthias Sieber <empower@manonthemat.com>
2013-12-14 12:53:39 -0800

    yummy lunch

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Send14ops::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
